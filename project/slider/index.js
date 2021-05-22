251959b9cf85d6419c3acdf27cc7c9df0cb6d83a
KatsiarynaKarneliuk <v281107@gmail.com>
2021-05-22 22:57:34 +0300

    slider was added

---
+++ project/slider/index.js
@@ -0,0 +1,45 @@
+function left(){
+    let left =Document.getElementByClassName('left-arrow');
+    return left;
+    }
+    
+function right(){
+    let left =Document.getElementByClassName('right-arrow');
+    return right;
+}
+/* Индекс слайда по умолчанию */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("photo");
+
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+  
+}
