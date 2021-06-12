c7ddd4e041d0b5ad5f14d80785aab244db87a70a
KatsiarynaKarneliuk <v281107@gmail.com>
2021-06-12 23:31:40 +0300

    fio17 was changed

--- project/FIO17/index.js
+++ project/FIO17/index.js
@@ -1,21 +1,17 @@
 function output(){
+    let spls=[];
     let fio=document.querySelector(".fio").value;
-    if (fio !=""){ 
-        lowFio=fio.toLowerCase().value;
-        return  lowFio;
-        }  
-    let spls=lowFio.split(" ");
-    el="";
-    for (el of spls){
-        let firstLetter = el.substring (0, 1); 
-        let el= firstLetter.toUpperCase() + el.slice(1).value;
-        return el;    
+    let fioLow = fio.toLowerCase();
+    spls = fioLow.split(" ");
+    for (let i = 0; i < spls.length; i++){
+        let firstLetter = spls.substring(0, 1); 
+        firstLetter.toUpperCase() + spls.slice(1).value;
         }
-    if(el == spls[0]){
+    if(spls[0]){
         document.getElementsByClassName("lname").innerHTML+=`Фамилия: ${spls[0]}`;}
-    else if(el == spls[1]){
+    else if(spls[1]){
         document.getElementsByClassName("fname").innerHTML+=` Имя: ${spls[1]}`;}
-    else if(el == spls[2]) {
+    else if(spls[2]) {
         document.getElementsByClassName("middle_name").innerHTML+=`Отчество: ${spls[2]}`;}           
     }
 
