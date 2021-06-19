8423157b4f0ca7c68dc2582a85ad9f991de2d5eb
KatsiarynaKarneliuk <v281107@gmail.com>
2021-06-19 20:14:01 +0300

    calc19 was added

--- project/calc19/index.js
+++ project/calc19/index.js
@@ -1,38 +1,39 @@
 
-function sum(a,b){   
-    return +a+ +b;   
+class Calculator{
+    static sum(a,b){   
+        return +a+ +b;   
+    }
+    static sub(a,b){
+        return a-b;
+    }
+    static mult(a,b){
+        return a*b;
+    }
+    static divis(a,b){
+        return a/b;
+    }
 }
-function sub(a,b){
-    return a-b;
-}
-function mult(a,b){
-    return a*b;
-}
-function divis(a,b){
-    return a/b;
-}
-
 function showSum(){
     let a=document.getElementById('firstNumber').value;  
     let b=document.getElementById('secondNumber').value;
-    document.getElementById('result').value = sum(a,b); 
+    document.getElementById('result').value = Calculator.sum(a,b); 
 }
 function showSub(){
     let a=document.getElementById('firstNumber').value;  
     let b=document.getElementById('secondNumber').value;
-    document.getElementById('result').value = sub(a,b); 
+    document.getElementById('result').value = Calculator.sub(a,b); 
 }
 function showMult(){
     let a=document.getElementById('firstNumber').value;  
     let b=document.getElementById('secondNumber').value;
-    document.getElementById('result').value = mult(a,b); 
+    document.getElementById('result').value = Calculator.mult(a,b); 
 }
 function showDivis(){
     let a=document.getElementById('firstNumber').value;  
     let b=document.getElementById('secondNumber').value;
-    document.getElementById('result').value = divis(a,b);
+    document.getElementById('result').value = Calculator.divis(a,b);
     if(b==0){
-        document.getElementById('result').value="Делить на '0' нельзя"
+    document.getElementById('result').value="Делить на '0' нельзя"
     } 
 }
 
